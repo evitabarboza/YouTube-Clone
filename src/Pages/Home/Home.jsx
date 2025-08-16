6a29dd51a2db3e07d62f6817c385b58f0582f5d3
evitabarboza <evitabarboza195@gmail.com>
2025-08-16 09:44:38 +0530

    Adding files till sidebar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+
+import './Home.css'
+
+const Home = ({sidebar}) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar} />
+    </>
+  )
+}
+
+export default Home
