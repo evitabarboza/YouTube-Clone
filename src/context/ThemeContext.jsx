41c08a8ede67455f131a33054ddb5bb6510d3726
evitabarboza <evitabarboza195@gmail.com>
2025-08-17 12:11:59 +0530

    Few changes made

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.className = theme; // add 'light' or 'dark' to <html>
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
